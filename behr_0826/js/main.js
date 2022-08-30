7bf6287818ebed3d197a824a94a6f42d2bb743c9
ahnpyojeong <ahnpyojeong@gmail.com>
2022-08-30 16:49:36 +0900

    1

--- behr_0826/js/main.js
+++ behr_0826/js/main.js
@@ -22,16 +22,21 @@ $(document).ready(function(){
         조건 2 - 스크롤 값이 0이면 header에 fixed 클래스 삭제
     */
     // ↓ 로딩했을 때 처음 한번만 실행
-    let scrolling = $(window).scrollTop()
-    console.log(scrolling)
-    $(window).scroll(function(){ /* 스크롤 할 때마다 실행 */
+    let scrolling
+    headerFixed() // 함수의 실행
+
+    /* ↓ 스크롤 할 때마다 실행 (스크롤 안하면 실행 X) */
+    $(window).scroll(function(){
+        headerFixed // 함수의 실행
+    })
+
+    function headerFixed(){ // 함수의 선언
         scrolling = $(window).scrollTop()
-        console.log(scrolling)
         if(scrolling > 0){
             $('header').addClass('fixed')
         }else{
             $('header').removeClass('fixed')
         }
-    })
+    }
 })
 
