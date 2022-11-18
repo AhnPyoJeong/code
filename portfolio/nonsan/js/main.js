ddedc82adc632683604e7041dcc36401153f70ed
ahnpyojeong <ahnpyojeong@gmail.com>
2022-11-18 18:00:29 +0900

    221118 포트폴리오 논산시청 service 모바일 시작

--- portfolio/nonsan/js/main.js
+++ portfolio/nonsan/js/main.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
 		prevEl: '.first .button_prev',  
 	},
 
-    }); // container swiper 종료
+    });
+
+
     const swiper_next = new Swiper('.container .second .popup', {
 
         effect: "slide",
@@ -44,7 +46,34 @@ $(document).ready(function(){
             prevEl: '.second .button_prev',  
         },
     
-        }); // container swiper 종료
+    });
+
+
+    const swiper_travel = new Swiper('.container .main .travel .group .popup', {
+
+        effect: "slide",
+    
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 5000,
+            disableOnInteraction: true,
+        },
+    
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.container .main .travel .group .controlbox .count', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',
+        },
+    
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.container .main .travel .group .controlbox .click .button_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.container .main .travel .group .controlbox .click .button_prev',  
+        },
+    
+    }); // container swiper 종료
+        
+
 
     /* visual popup의 정지/재생버튼 */
     $('.container .first .controlbox .button_pause').on('click', function(){
@@ -68,6 +97,19 @@ $(document).ready(function(){
             $(this).text('일시정지')
         }else{ /* 버튼의 상태가 일시정지 모양 - 현재 재생상태 - 일시정지 기능을 실행 */
             swiper_next.autoplay.stop()
+            $(this).addClass('play')
+            $(this).text('재생')
+        }
+    })
+
+    $('.container .main .travel .group .controlbox .controlbox_wrap .button_pause').on('click', function(){
+        let popStatus = $(this).hasClass('play')
+        if(popStatus == true){ /* 버튼의 상태가 play 모양 - 현재 정지상태 - 재생기능을 실행 */
+            swiper_travel.autoplay.start()
+            $(this).removeClass('play')
+            $(this).text('일시정지')
+        }else{ /* 버튼의 상태가 일시정지 모양 - 현재 재생상태 - 일시정지 기능을 실행 */
+            swiper_travel.autoplay.stop()
             $(this).addClass('play')
             $(this).text('재생')
         }
